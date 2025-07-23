c6111d3b9f397eb34f86d60661301ff739bc0c49
Aline Maria <aline.tecnologiainformacao@gmail.com>
2025-07-23 17:12:00 -0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,10 @@ function Layout() {
   return (
     <div className="min-h-screen bg-white text-slick font-serif">
       <header className="bg-white text-slick shadow-md">
-        {/* seu conteúdo do header aqui */}
       </header>
 
       <main className="container mx-auto px-4 py-8 bg-white">
         <Routes>
-          {/* suas rotas aqui */}
         </Routes>
       </main>
 
